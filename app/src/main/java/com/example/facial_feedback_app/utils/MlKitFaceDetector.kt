67928d2f31cffe25245013085e58a2078d153c8a
ronit-khalate <ronitkhalate243@gmail.com>
2024-01-08 09:48:27 +0530

    Bounding of face checked Each face now being checked if its bounds are in the camera If face is out of camera view or half then it will be discardedQ

--- app/src/main/java/com/example/facial_feedback_app/utils/MlKitFaceDetector.kt
+++ app/src/main/java/com/example/facial_feedback_app/utils/MlKitFaceDetector.kt
@@ -3,6 +3,7 @@ package com.example.facial_feedback_app.utils
 import android.graphics.Bitmap
 import android.util.Log
 import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.Face
 import com.google.mlkit.vision.face.FaceDetection
 import com.google.mlkit.vision.face.FaceDetectorOptions
 import javax.inject.Inject
@@ -12,9 +13,10 @@ class MlKitFaceDetector @Inject constructor() {
 
     // Face Detection model parameter
     private val faceDetectorOptions = FaceDetectorOptions.Builder()
-        .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
+        .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST)
         .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_NONE)
         .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_NONE)
+        .setMinFaceSize(100F)
         .build()
 
     // Face Detector
@@ -29,24 +31,28 @@ class MlKitFaceDetector @Inject constructor() {
         var facesList:MutableList<Bitmap> = mutableListOf()
         mlKitFaceDetector.process(image)
             .addOnSuccessListener { faces->
-
                 Log.d("Success", "${faces.size}")
-                faces.forEach {
-                    val boundingBox = it.boundingBox
-                    var face = Bitmap.createBitmap(
-                            bitmap,
-                            boundingBox.left,
-                            boundingBox.top,
-                            boundingBox.width(),
-                            boundingBox.height()
-                    )
+                faces.forEach {face->
+                    val boundingBox = face.boundingBox
 
 
-                    //TODO: Use resize function before passing images to model
-                    facesList.add(face.toGrayscale())
+                    // Ensure that bounding box coordinates are within the bounds of the original bitmap
+                    val x :Int= boundingBox.left.coerceAtLeast(0)
+                    val y :Int= boundingBox.top.coerceAtLeast(0)
+                    val width = boundingBox.width().coerceAtMost(bitmap.width - x)
+                    val height = boundingBox.height().coerceAtMost(bitmap.height - y)
+
+                    // Create a new Bitmap with the corrected dimensions
+                    if (width > 0 && height > 0) {
+                        val croppedFace = Bitmap.createBitmap(bitmap, x, y, width, height)
+                        facesList.add(croppedFace.toGrayscale().resize(100,100))
+                    }
+
+
                 }
 
                 addFaceToList(facesList)
+                facesList.clear()
 
 
             }
