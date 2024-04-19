008518b96ed0bc6a896bb296e50f5f96d7039641
ronit-khalate <ronitkhalate243@gmail.com>
2024-04-19 15:52:39 +0530

    Created Data Analyser class
    This class is responisble for analyzing emotions of each frame and converting it into timeseries data and other data format which will we usefull for data analysis

--- app/src/main/java/com/example/facial_feedback_app/feature_record/domain/data_analysis/DataAnalyzer.kt
+++ app/src/main/java/com/example/facial_feedback_app/feature_record/domain/data_analysis/DataAnalyzer.kt
@@ -1,8 +1,79 @@
-package com.example.facial_feedback_app.feature_record.domain
+package com.example.facial_feedback_app.feature_record.domain.data_analysis
 
-class DataAnalyzer {
+import android.content.Context
+import android.util.Log
+import androidx.camera.core.ImageProxy
+import androidx.camera.view.LifecycleCameraController
+import androidx.core.content.ContextCompat
+import com.example.facial_feedback_app.feature_record.domain.Emotions
+import com.example.facial_feedback_app.feature_record.presentation.camera.CameraViewModel
+import com.example.facial_feedback_app.feature_record.presentation.camera.state.RecordingState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import javax.inject.Inject
+
+
+class DataAnalyzer @Inject constructor() {
+
+
+    var overAllEmotions:MutableList<Long> = mutableListOf()
+        private set
+
+    // Time series Emotion collection
+    val timeSeriesEmotionCollection:MutableMap<Long,List<Long>> = mutableMapOf()
 
 
 
-    // t
+    /**
+    * This method gives time series of particular emotions
+    * Need Emotions enums object
+    * */
+    fun getEmotionTimeSeriesData(emotion: Emotions): Map<Long, Long> {
+
+
+
+        return timeSeriesEmotionCollection.map {
+            it.key to it.value[emotion.ordinal]
+        }.toMap()
+
+    }
+
+
+    fun updateData(allEmotionMap:List<Float>){
+        
+    }
+
+
+    fun imageProxyFlow(cameraController: LifecycleCameraController,context:Context,viewModel: CameraViewModel):Flow<ImageProxy> = callbackFlow{
+
+        cameraController.setImageAnalysisAnalyzer(ContextCompat.getMainExecutor(context)){imageProxy: ImageProxy ->
+
+            Log.d("recording", cameraController.isRecording.toString())
+
+            if(viewModel.cameraModeState.recordingState is RecordingState.Started && cameraController.isRecording){
+
+                trySend(imageProxy)
+            }
+
+
+//            imageProxy.use {
+//                if(viewModel.cameraModeState.recordingState is RecordingState.Started && cameraController.isRecording){
+//
+//                    viewModel.mlKitFaceDetector.getFacesFromCapturedImage(it.toRotatedBitmap()){emotionMapOfFrame:Map<Int,Float>->
+//
+//
+//
+//                        viewModel.updateEmotionSumMap(emotionMapOfFrame)
+//
+//
+//
+//                    }
+//                }
+//            }
+
+
+
+        }
+
+    }
 }
