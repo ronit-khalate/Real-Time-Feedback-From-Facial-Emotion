5ae1e33dee5c23f125c47310d02c0277d1447dd8
ronit-khalate <ronitkhalate243@gmail.com>
2024-04-19 15:50:08 +0530

    Created EmotionTimeData class
    it represents data point in timeseries data of emotions of at perticular time in video

--- app/src/main/java/com/example/facial_feedback_app/feature_record/domain/data_analysis/EmotionTimeData.kt
+++ app/src/main/java/com/example/facial_feedback_app/feature_record/domain/data_analysis/EmotionTimeData.kt
@@ -1,4 +1,12 @@
 package com.example.facial_feedback_app.feature_record.domain.data_analysis
 
-class EmotionTimeData {
-}
+
+/*
+* This data class represents the emotion captured at give time
+*
+*
+* */
+data class EmotionTimeData(
+        val time:Long,
+        val emotions:List<Long>
+)
