a7817b76a1b3fa322dbb3f91ef4b1f02c21a1719
ronit-khalate <ronitkhalate243@gmail.com>
2024-04-19 21:24:04 +0530

    StoageScreen code deleted

--- app/src/main/java/com/example/facial_feedback_app/feature_record/presentation/storage/StorageScreen.kt
+++ app/src/main/java/com/example/facial_feedback_app/feature_record/presentation/storage/StorageScreen.kt
@@ -1,23 +1,6 @@
 package com.example.facial_feedback_app.feature_record.presentation.storage
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
-import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
-import androidx.compose.foundation.lazy.staggeredgrid.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.unit.dp
 import com.example.facial_feedback_app.feature_record.presentation.camera.CameraViewModel
 
 
@@ -26,41 +9,4 @@ fun StorageReview(
     cameraViewModel: CameraViewModel
 ){
 
-    val bitmaplist = cameraViewModel.bitmaps.collectAsState().value
-    LazyVerticalStaggeredGrid(
-            columns = StaggeredGridCells.Fixed(2),
-            horizontalArrangement = Arrangement.spacedBy(16.dp),
-            verticalItemSpacing = 16.dp,
-    ){
-        items(
-                items = bitmaplist
-        ){ bitmap ->
-
-            Column(
-
-                    modifier = Modifier,
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-
-            ) {
-                Image(
-                        bitmap = bitmap.image.asImageBitmap(),
-                        modifier = Modifier
-                            .clip(RoundedCornerShape(12.dp))
-                            .height(100.dp)
-                            .width(100.dp),
-                        contentDescription =""
-                )
-
-                Spacer(modifier = Modifier.height(5.dp))
-
-                for (key in bitmap.emotion.keys){
-
-                    Text(text = "${key} = ${bitmap.emotion[key].toString().substring(0,5)}%")
-                }
-
-            }
-
-        }
-    }
 }
