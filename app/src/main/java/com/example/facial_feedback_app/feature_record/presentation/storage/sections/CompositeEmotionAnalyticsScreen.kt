7b1e227afe1aecfb0733455a84b05a9153c46d65
ronit-khalate <ronitkhalate243@gmail.com>
2024-05-24 02:12:23 +0530

    composite emotions screen to compare emotion data created

---
+++ app/src/main/java/com/example/facial_feedback_app/feature_record/presentation/storage/sections/CompositeEmotionAnalyticsScreen.kt
@@ -0,0 +1,9 @@
+package com.example.facial_feedback_app.feature_record.presentation.storage.sections
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CompositeEmotionAnalyticsScreen(modifier: Modifier = Modifier) {
+    
+}
